562c8c63a591ee356318748f183750027507c3df
Sanaz <sanaz.azh@gmail.com>
2023-04-09 15:45:53 -0400

    Add support for Set Expiry

--- app/server.go
+++ app/server.go
@@ -9,11 +9,27 @@ import (
 	"net"
 	"os"
 	"strconv"
+	"time"
 )
 
 func exitWithError(err error) {
 	fmt.Println(err.Error())
 	os.Exit(1)
+}
+
+type TimeValue struct {
+	value     string
+	createdAt time.Time
+	ex        int
+}
+
+func (v TimeValue) isExpired() bool {
+	if v.ex == 0 {
+		return false
+	}
+	expiry := v.createdAt.Add(time.Millisecond * time.Duration(v.ex))
+	return expiry.Before(time.Now())
+
 }
 
 func main() {
@@ -24,7 +40,8 @@ func main() {
 		exitWithError(fmt.Errorf("failed to bind to port 6379: %v", err))
 	}
 
-	keyTable := make(map[string]string)
+	// keyTable := make(map[string]string)
+	expiryTable := make(map[string]TimeValue)
 
 	for {
 		conn, err := l.Accept()
@@ -32,11 +49,11 @@ func main() {
 			exitWithError(fmt.Errorf("Error accepting connection: %v", err))
 		}
 
-		go handleConnection(conn, keyTable)
+		go handleConnection(conn, expiryTable)
 	}
 }
 
-func handleConnection(conn net.Conn, keyTable map[string]string) {
+func handleConnection(conn net.Conn, expiryTable map[string]TimeValue) {
 	defer conn.Close()
 
 	for {
@@ -60,20 +77,45 @@ func handleConnection(conn net.Conn, keyTable map[string]string) {
 		case "echo":
 			conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(args[0].String), args[0].String)))
 		case "set":
-			keyTable[args[0].String] = args[1].String
-			fmt.Println("keyTable", keyTable)
-			conn.Write([]byte("+OK\r\n"))
+			handleSet(args, expiryTable, conn)
 		case "get":
-			val, ok := keyTable[args[0].String]
-			if ok {
-				conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(val), val)))
-			} else {
-				conn.Write([]byte("$-1\r\n"))
-			}
-
+			handleGet(args, expiryTable, conn)
 		default:
 			conn.Write([]byte(fmt.Sprintf("-Error unknown command %s\r\n", command)))
 		}
+	}
+}
+
+func handleSet(args []Val, expiryTable map[string]TimeValue, conn net.Conn) {
+	if len(args) < 2 {
+		return
+	}
+
+	key := args[0].String
+	value := TimeValue{args[1].String, time.Now(), 0}
+	if len(args) == 4 && args[2].String == "px" {
+		ms, err := strconv.Atoi(args[3].String)
+		if err != nil {
+			fmt.Println("Failed to parse. %s", ms)
+			return
+		}
+		value.ex = ms
+	}
+
+	expiryTable[key] = value
+
+	conn.Write([]byte("+OK\r\n"))
+}
+
+func handleGet(args []Val, expiryTable map[string]TimeValue, conn net.Conn) {
+	if len(args) != 1 {
+		return
+	}
+	value, ok := expiryTable[args[0].String]
+	if ok && !value.isExpired() {
+		conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(value.value), value.value)))
+	} else {
+		conn.Write([]byte("$-1\r\n"))
 	}
 }
 
@@ -113,7 +155,6 @@ func parseVal(val Val) []byte {
 
 func parseRESP(b *bufio.Reader) (Val, error) {
 	firstByte, err := b.ReadByte()
-	// println("firstByte is: ", firstByte)
 	if err != nil {
 		return Val{}, err
 	}
@@ -140,7 +181,6 @@ func parseArray(b *bufio.Reader) (Val, error) {
 	}
 	sizeByte = bytes.TrimSpace(sizeByte)
 	size, err := strconv.Atoi(string(sizeByte))
-	fmt.Println("size of array is: ", size)
 	if err != nil {
 		exitWithError(err)
 	}
@@ -148,7 +188,6 @@ func parseArray(b *bufio.Reader) (Val, error) {
 	result := make([]Val, 0)
 	for i := 0; i < size; i++ {
 		ele, err := parseRESP(b)
-		fmt.Println("ele inside loop: ", ele)
 		if err != nil {
 			return Val{}, err
 		}
